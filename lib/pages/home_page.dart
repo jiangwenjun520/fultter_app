5a7b58652fdb4b7b98490b6418c2d541eca6a27a
jiangwenjun520 <382951322@qq.com>
2022-03-18 17:53:52 +0800

    ##

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -11,85 +11,69 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final PageController _controller = PageController(
-    initialPage: 0,
-  );
-  final List<String> _imageList = [
-    "https://pages.ctrip.com/commerce/promote/20180718/yxzy/img/640sygd.jpg",
-    "https://dimg04.c-ctrip.com/images/700u0r000000gxvb93E54_810_235_85.jpg",
-    "https://dimg04.c-ctrip.com/images/700c10000000pdili7D8B_780_235_57.jpg",
-  ];
-  double appBarAlpha = 0;
-
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        body: Stack(
-      children: <Widget>[
-        MediaQuery.removePadding(
-            removeTop: true,
-            context: context,
-            child: NotificationListener(
-              onNotification: (scrollNotification) {
-                if (scrollNotification is ScrollNotification &&
-                    scrollNotification.depth == 0) {
-                  _onSroll(scrollNotification.metrics.pixels);
-                }
-                return true;
-              },
-              child: ListView(
-                children: <Widget>[
-                  Container(
-                    height: 160,
-                    child: Swiper(
-                        itemCount: _imageList.length,
-                      autoplay: true,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Image.network(
-                          _imageList[index],
-                          fit: BoxFit.fill,
-                        );
-                      },
-                      pagination: const SwiperPagination(),
-                    ),
-                  ),
-                  Container(
-                    height: 1000,
-                    child: const ListTile(
-                      title: Text('哈哈'),
-                    ),
-                  )
-                ],
-              ),
-            )),
-        Opacity(
-          child: Container(
-            height: 80,
-            decoration: BoxDecoration(color: Colors.white),
-            child: const Center(
-              child: Padding(
-                padding: EdgeInsets.only(top: 20),
-                child: Text("首页"),
-              ),
-            ),
-          ),
-          opacity: appBarAlpha,
-        ),
-      ],
-    ));
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 1,
+        title: HomeAppBar(),
+      ),
+      body: Container(),
+    );
   }
+}
 
-  void _onSroll(offset) {
-    double alpha = offset / APPBAR_SCROLL_OFFSET;
-    if (alpha < 0) {
-      alpha = 0;
-    } else if (alpha > 1) {
-      alpha = 1;
-    }
-    setState(() {
-      appBarAlpha = alpha;
-    });
-    print(alpha);
+class HomeAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+            flex: 1,
+            child: InkWell(
+              child: Container(
+                alignment: Alignment.center,
+                height: 40,
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.black12,
+                ),
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const <Widget>[
+                        Icon(
+                          Icons.search,
+                          color: Colors.black38,
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Text(
+                          "Search Products",
+                          style: TextStyle(color: Colors.black38),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+
+            )),
+        Expanded(flex: 0, child:IconButton(
+          icon: Icon(Icons.add_shopping_cart),
+          color: Colors.black54,
+          onPressed: (){
+            //跳转购物车
+
+          },
+        ),)
+      ],
+    );
   }
 }
--- lib/pages/seach_page.dart
+++ lib/pages/seach_page.dart
@@ -1,29 +1,52 @@
 import 'package:flutter/material.dart';
 
-class SeachPage extends StatefulWidget{
+class SeachPage extends StatefulWidget {
   const SeachPage({Key? key}) : super(key: key);
 
   @override
-  _SeachPageState createState()=>_SeachPageState();
-
-
-
+  _SeachPageState createState() => _SeachPageState();
 }
-class _SeachPageState extends State<SeachPage>{
-  final PageController _controller =PageController(
+
+class _SeachPageState extends State<SeachPage> {
+  List list = [];
+  final PageController _controller = PageController(
     initialPage: 0,
   );
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    var display1;
     return Scaffold(
-      body: Container(
-        padding:EdgeInsets.all(10),
-        child: const Center(
-            child: Text("搜索")
+        appBar: AppBar(
+          title: const Text("搜索"),
         ),
-      ),
+        restorationId: "fdakgaewa",
+        body: Container(
+          child: Container(
+            height: 300,
+            width: 300,
+            color: Colors.deepOrange,
+            child: Column(
+              children: <Widget>[
+                Column(
+                    children:
+                    this.list.map((e) => Chip(label: Text("${e}"))).toList(),
+                ),
+                  const SizedBox(height: 20),
+                RaisedButton(
+                  child: const Text("点击"),
+                  onPressed: (){
+                    setState(() {
+                      list.add("点击新增一条数据");
+                    });
+                  },
+                )
+                
+              ],
+            ),
+          ),
+        )
     );
   }
-
 }
